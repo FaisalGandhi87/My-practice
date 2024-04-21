0ef05b3c07d9590738469d3f81c4d8c9f7569335
FaisalGandhi87 <159810032+FaisalGandhi87@users.noreply.github.com>
2024-04-21 21:43:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,110 @@
+// var num = (1)
+// var newNum = --num
+// alert(newNum)
+// var firstnumber =+prompt("What is your num");
+// var lastnumber = +prompt("What is your last num");
+// var sum = (firstnumber+lastnumber);
+// alert(sum)
+// alert("firstname" + "lastname")
+// // var firstnumber =(4);
+// var lastnumber = (5);
+// var sum = ("2" +  "2");
+// alert(sum)
+// console.log(fullname)
+// var firstname =prompt("What is your first name");
+// var lastname = prompt("What is your last name");
+// var sum = (firstname + " " + lastname);
+// alert(sum)
+// console.log(sum)
+// function sum(){var firstnumber =+prompt("What is your num");
+// var lastnumber = +prompt("What is your last num");
+// var sum = (firstnumber+lastnumber);
+// alert(sum)
+// // console.log(sum)
+// }
+// sum()
+// sum()
+// sum()
+
+// var cleanistCity = ["Karachi","Lahore","islamabad"];
+// var favCity = prompt("Enter your city");
+// for (i= 0; i< cleanistCity.length; i++) {if(favCity===cleanistCity[0]){
+//     alert("you live in clean city")}
+// else{alert("you live in unclean city")   
+// }
+// }
+// var cleanistCity=["Karachi","Lahore","Quetta","Peshawar"]
+// var cleanCity2=cleanistCity.slice(0,1)
+// console.log(cleanCity2)
+// var table=[1,2,3,4,5,6,7,8,9,10,11,12];
+// for(var i=1;i<12;i++){
+//     console.log(2+"x "+i+" = "+2*i,"<br>")
+// }
+// var userInput=+prompt("Enter your number")
+// for(var i=1;i<=10;i++){
+//     console.log(userInput+ "x" +i+" = "+userInput*i,"<br>")
+// }
+// var names = ["Ali","Hasan","Atif","Hussain"]
+// var userNames = prompt("Enter your name")
+// if(userNames===names){alert("Congratulation")}
+// else{alert("Try again")}
+// var userAge = +prompt("Your Age")
+// if(userAge>18){alert("You are old enough")}
+// else{alert("Too Young")}
+// var password = ["abc1234"]
+// var userInput =prompt("Enter password")
+// if(userInput==password){alert("correct")}
+// else{alert("Wrong password")}
+
+// var userInput =prompt("Enter number")
+// if(userInput%2==0){alert("Even")}
+// // else{alert("odd")}
+
+// var cities = ["karachi","lahore","peshawar","quetta"]
+// var userInp=prompt("Enter city")
+// var match=false
+// for (var i=0;i<cities.length;i++){if(userInp==cities[i])
+//     {match=true
+//     alert("Clean city")
+//     break    
+// }
+// }
+// if(match==false){alert("unclean city")}
+
+// var cities = ["karachi","lahore","peshawar","quetta"]
+// var country = ["pakistan"]
+// for (var i=0;i<cities.length;i++){
+//     console.log(cities[i]+ " "+ country)
+// }  
+
+// var days = ["Mon","Tues","Wed","Thurs","Fri","Sat","Sun"]
+// var months = ["Jan","feb","mar","apr","may","jun"];
+// var years = [2022,2023,2024]
+// for (var i=0;i<days.length;i++){for (var j=0;j<months.length;j++) {for(var k=0;k<years.length;k++)
+// console.log(days[i]+ " "+ months[j]+ " "+years[k])
+// }  
+// }
+// var userData = prompt("Enter your name")
+// userData = userData.toUpperCase()
+// console.log(userData)
+
+// var userData1 = prompt("Enter your name")
+// userData1 = userData1.toLowerCase()
+// console.log(userData1)
+
+// var cities = ["karachi","lahore","peshawar","quetta"]
+// var userInp=prompt("Enter city")
+// userInp = userInp.toLowerCase()
+// var match=false
+// for (var i=0;i<cities.length;i++){if(userInp==cities[i])
+//     {match=true
+//     alert("Clean city")
+//     break    
+// }
+// }
+// if(match==false){alert("unclean city")}
+
+// var month = prompt("Enter month")
+// var chairMonth = month.length;
+// if(chairMonth>3){monthAbbre=month.slice(0,4)}
+// console.log(monthAbbre)
